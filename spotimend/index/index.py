bc2134aba04f22da4031a900f41c1093e5d9a269
Sellers Crisp <sellershcrisp@gmail.com>
2021-01-28 15:31:25 -0600

    Added index with spotify auth/login

---
+++ spotimend/index/index.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template, url_for
+from spotimend.api.spotify_auth import login
+
+index_blueprint = Blueprint('index', __name__, template_folder='templates')
+
+
+@index_blueprint.route('/')
+def index():
+    return render_template('index.html')

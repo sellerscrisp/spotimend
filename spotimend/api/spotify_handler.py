ad1c02968d6f749c9d325c8d6e0ac0e04353cc28
Sellers Crisp <sellershcrisp@gmail.com>
2021-01-31 17:23:39 -0600

    Added new methods for playback changes using POST requests, fixed other methods returning errors.

--- spotimend/api/spotify_handler.py
+++ spotimend/api/spotify_handler.py
@@ -89,23 +89,63 @@ class SpotifyHandler:
         """
 
         user_curr_playing_endpoint = f'{self.SPOTIFY_API_URL}/me/player/currently-playing?market=US'
-        curr_playing = json.loads(requests.get(
+        curr = json.loads(requests.get(
             user_curr_playing_endpoint, headers=auth_header).text)
-        curr_playing = curr_playing[1]['item']
+        curr = curr
 
         curr_data = []
 
-        for curr in curr_playing:
-            curr_data.append({
-                # 'track_img': curr['album']['images'][0]['url'],
-                # 'track_artist': curr['artists']['name'],
-                # 'track_name': curr['name'],
-                # 'track_id': curr['id'],
-                'track_uri': curr['uri'],
-                # 'track_url': curr['external_urls']['spotify'],
-                # 'track_preview': curr['preview_url'],
-            })
+        curr_data.append({
+            'track_artist': curr['item']['artists'][0]['name'],
+            'track_name': curr['item']['name'],
+            'track_id': curr['item']['id'],
+            'track_img': curr['item']['album']['images'][0]['url'],
+            # 'track_url': curr['external_urls']['spotify'],
+            # 'track_preview': curr['preview_url'],
+        })
+
         return curr_data
+
+    def post_next_track(self, auth_header):
+        """"""
+
+        next_track_endpoint = f'{self.SPOTIFY_API_URL}/me/player/next'
+        next_track = requests.post(next_track_endpoint, headers=auth_header)
+
+        return next_track
+
+    def post_previous_track(self, auth_header):
+        """"""
+
+        previous_track_endpoint = f'{self.SPOTIFY_API_URL}/me/player/previous'
+        previous_track = requests.post(
+            previous_track_endpoint,
+            headers=auth_header
+        )
+
+        return previous_track
+
+    def post_pause_track(self, auth_header):
+        """"""
+
+        pause_track_endpoint = f'{self.SPOTIFY_API_URL}/me/player/pause'
+        pause_track = requests.post(
+            pause_track_endpoint,
+            header=auth_header
+        )
+
+        return pause_track
+
+    def play_track(self, auth_header):
+        """"""
+
+        play_track_endpoint = f'{self.SPOTIFY_API_URL}/me/player/play'
+        play_track = requests.post(
+            play_track_endpoint,
+            header=auth_header
+        )
+
+        return play_track
 
     def get_recently_played_data(self, auth_header):
         """Get user's recently played songs.
@@ -122,7 +162,10 @@ class SpotifyHandler:
 
         for track in recently_played:
             recently_data.append({
-                'artist_name': track['track']['album']['artists'][0]['name'],
+                'artist_name': track['track']['artists'][0]['name'],
+                'track_name': track['track']['name'],
+                'track_img': track['track']['album']['images'][0]['url'],
+                'track_popularity': track['track']['popularity'],
             })
 
         return recently_data

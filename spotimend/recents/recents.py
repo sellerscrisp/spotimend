324831185a57456204377b9dbc9f045a2c840427
Sellers Crisp <sellershcrisp@gmail.com>
2021-01-31 17:18:14 -0600

    Added recents page with currently playing and most recently played tracks

---
+++ spotimend/recents/recents.py
@@ -0,0 +1,52 @@
+import json
+import requests
+from flask import Blueprint, render_template, session, url_for, request, redirect
+from spotimend.api.spotify_handler import SpotifyHandler
+
+recents_blueprint = Blueprint('recents', __name__, template_folder='templates')
+spotify = SpotifyHandler()
+
+
+@recents_blueprint.route('/recents', methods=['GET', 'POST'])
+def recents():
+    """"""
+
+    authorization_header = session['authorization_header']
+
+    if request.method == 'GET':
+        profile_data = spotify.get_user_profile_data(
+            authorization_header
+        )
+
+        user_display_name, user_id = profile_data['display_name'], profile_data['id']
+        session['user_id'], session['user_display_name'] = user_id, user_display_name
+
+        recently_played = spotify.get_recently_played_data(
+            authorization_header
+        )
+
+        curr_playing = spotify.get_currently_playing_data(
+            authorization_header
+        )
+
+        return render_template(
+            'recents.html',
+            profile_data=profile_data,
+            user_display_name=user_display_name,
+            recently_played=recently_played,
+            curr_playing=curr_playing,
+        )
+
+    return render_template('recents.html')
+
+
+@recents_blueprint.route('/recents/next', methods=['GET', 'POST'])
+def next_track():
+    """"""
+
+    authorization_header = session['authorization_header']
+
+    # if request.method == 'GET':
+    spotify.post_next_track(authorization_header)
+
+    return redirect('/recents')

25abd180eff97b3f4f22f736d8d43c744e96df85
Sellers Crisp <sellershcrisp@gmail.com>
2021-02-15 15:24:13 -0600

    Added port=None in run due to callback error

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ production = os.environ.get('PRODUCTION', False)
 if __name__ == '__main__':
     if production:
         app.jinja_env.cache = {}
-        app.run(debug=False)
+        app.run(debug=False, port=None)
     else:
         app.jinja_env.cache = {}
         app.run(host='127.0.0.1', port=5000, debug=True)
